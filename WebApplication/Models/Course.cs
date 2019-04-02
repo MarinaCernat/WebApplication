9ba5efb98cdeec2393d44a4569143a2b7e664327
DESKTOP-JH8DCM3\Marina Cernat <marina.butterfly95@gmail.com>
2019-04-03 00:12:41 +0300

    Primul commit

---
+++ WebApplication/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Course
+    {
+        [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
+        
+        [System.ComponentModel.DataAnnotations.Display(Name = "Number")]
+        public int CourseID { get; set; }
+       
+        [System.ComponentModel.DataAnnotations.StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        
+        [System.ComponentModel.DataAnnotations.Range(0, 5)]
+        public int Credits { get; set; }
+        
+        public int DepartmentID { get; set; }
+        
+        public virtual Department Department { get; set; }
+        
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual ICollection<Instructor> Instructors { get; set; }
+    }
+}
---
+++ WebApplication/Models/Department.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [System.ComponentModel.DataAnnotations.DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [System.ComponentModel.DataAnnotations.Schema.Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        [System.ComponentModel.DataAnnotations.DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [System.ComponentModel.DataAnnotations.DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [System.ComponentModel.DataAnnotations.Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        public int? InstructorID { get; set; }
+
+        public virtual Instructor Administrator { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
